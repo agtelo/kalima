a7c284bb6a415bea4f3e10e17ee5f40299c5711b
agtelo <alantelo1987@gmail.com>
2021-08-15 22:05:41 -0300

    correciones en ItemListConteiner

--- src/components/ItemListContainer/ItemList/ItemList.js
+++ src/components/ItemListContainer/ItemList/ItemList.js
@@ -1,59 +1,88 @@
-const ItemList = () => {
-  const items = [
-    {
-      id: 1,
-      Title: "Guitarra Electrica",
-      Marca: "Fender",
-      Modelo: "Stratocaster",
-      Precio: 25000,
-    },
-    {
-      id: 2,
-      Title: "Guitarra Electrica",
-      Marca: "Fender",
-      Modelo: "Telecaster",
-      Precio: 35000,
-    },
-    {
-      id: 3,
-      Title: "Guitarra Electrica",
-      Marca: "Gibson",
-      Modelo: "Les Paul Clasic",
-      Precio: 45000,
-    },
-    {
-      id: 4,
-      Title: "Guitarra Electrica",
-      Marca: "Gibson",
-      Modelo: "SG",
-      Precio: 35000,
-    },
-    {
-      id: 5,
-      Title: "Guitarra Electrica",
-      Marca: "Ibanez",
-      Modelo: "GRx 70Qa",
-      Precio: 50000,
-    },
-    {
-      id: 6,
-      Title: "Guitarra Electrica",
-      Marca: "Jackson",
-      Modelo: "Dinky JS12",
-      Precio: 25000,
-    },
-  ];
-
-  const getItemList = () => {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve(items);
-      }, 2000);
-    });
-  };
-  getItemList().then((items) => console.log(items));
-
-  return <div></div>;
-};
-
-export default ItemList;
+export const items = [
+  {
+    id: 1,
+    Product_type: "Card Slider",
+    Brand: "Walla",
+    Model: "Black",
+    Price: 2050,
+    Stock: 15,
+    Img_product: (
+      <img src="/images/product/card-sliders/slider-black-frente.jpg" alt="" />
+    ),
+  },
+  {
+    id: 2,
+    Product_type: "Card Slider",
+    Brand: "Walla",
+    Model: "Fire",
+    Price: 2050,
+    Stock: 4,
+    Img_product: (
+      <img src="/images/product/card-sliders/slider-fire-frente.jpg" alt="" />
+    ),
+  },
+  {
+    id: 3,
+    Product_type: "Card Slider",
+    Brand: "Walla",
+    Model: "Silver",
+    Price: 2050,
+    Stock: 2,
+    Img_product: (
+      <img src="/images/product/card-sliders/slider-silver-frente.jpg" alt="" />
+    ),
+  },
+  {
+    id: 4,
+    Product_type: "Card Slider",
+    Brand: "Walla",
+    Model: "Marino",
+    Price: 2050,
+    Stock: 7,
+    Img_product: (
+      <img src="/images/product/card-sliders/slider-marino-frente.jpg" alt="" />
+    ),
+  },
+  {
+    id: 5,
+    Product_type: "Card Slider",
+    Brand: "Walla",
+    Model: "Cubic Bx",
+    Price: 2750,
+    Stock: 10,
+    Img_product: <img src="/images/product/card-sliders/cubic-bx.jpg" alt="" />,
+  },
+  {
+    id: 6,
+    Product_type: "Card Slider",
+    Brand: "Walla",
+    Model: "Cubic Black",
+    Price: 2750,
+    Stock: 3,
+    Img_product: (
+      <img src="/images/product/card-sliders/cubic-black.jpg" alt="" />
+    ),
+  },
+  {
+    id: 7,
+    Product_type: "Card Slider",
+    Brand: "Walla",
+    Model: "Marino Net",
+    Price: 2750,
+    Stock: 3,
+    Img_product: (
+      <img src="/images/product/card-sliders/net-marino.jpg" alt="" />
+    ),
+  },
+  {
+    id: 8,
+    Product_type: "Card Slider",
+    Brand: "Walla",
+    Model: "Overlock Grey",
+    Price: 2750,
+    Stock: 3,
+    Img_product: (
+      <img src="/images/product/card-sliders/overlook-grey.jpg" alt="" />
+    ),
+  },
+];
